bbafc589ee9e320b82bfb016d3ea56ef79d90788
Hamza Siddiqui <hamza.siddiqui4747@gmail.com>
2024-11-01 16:28:33 +0500

    .

--- controller.py
+++ controller.py
@@ -2,22 +2,28 @@ import pandas as pd
 import pyexcel as px
 
 def extractCSV(file):
+    if not file.endswith(".csv"):
+        return {
+            'status': False,
+            'message': "Please provide a CSV file only."
+        }
+    
     try:
         df = pd.read_csv(file)
-        return df
+        return {
+            'status': True,
+            'data': df
+        }
     except Exception as e:
-        if not file.endswith(".csv"):
-            return "Provide CSV file only"
-        
-        return f"There is error in extracted data from CSV file:\n{e}"
-        
-    
+        return {
+            'status': False,
+            'message': f"There was an error extracting data from the CSV file:\n{e}"
+        }
 
 def intoExcel(data, dest):
     try:
         data_dict = data.to_dict(orient='records')
         px.save_as(records=data_dict, dest_file_name=dest)
-        return True
+        return f"Your file has been successfully saved to: {dest}"
     except Exception as e:
         return f"There is an issue saving to Excel:\n{e}"
-    
--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import sys
 import controller as ct
 
-args = sys.argv
+if len(sys.argv) != 3:
+    print("Type: python main.py /dir/example.csv /dest/your_file.xlsx")
+    sys.exit(1)
 
-data = ct.extractCSV(args[1])
+args = sys.argv[1:]
 
-excellRes = ct.intoExcel(data, args[2])
-
+data = ct.extractCSV(args[0])
+if data['status']:
+    excellRes = ct.intoExcel(data['data'], args[1])
+    print(excellRes)
+else:
+    print(data['message'])
